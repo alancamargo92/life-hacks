cb9706476ac2d3e3c65effdcf5a183b78b5d8530
C5255008 <alan.camargo@sap.com>
2017-06-16 18:08:13 -0300

    First damn commit

---
+++ README.md
@@ -0,0 +1,10 @@
+###SCRIPTS###
+
+Scripts to make life less repetitive and boring.
+Well, all scripts are written for this reason anyway.
+Yours sincerely,
+
+[Your ruler](alcam.ukdev@gmail.com)
+
+PS.: Don't you ever commit rubbish here or else I will look for you, I will find you and I will kill you!
+
---
+++ rename_files.sh
@@ -0,0 +1,34 @@
+# Author: Alan Camargo
+
+#!/bin/bash
+
+if [ "$#" != 4 ]; then
+	echo "You must pass the following 4 arguments: target directory, extension (i.e.: *.c), name to replace, new name"
+	exit
+fi
+
+DIR_NAME="$1"
+echo "Target directory: ${DIR_NAME}"
+cd DIR_NAME
+
+EXTENSION="$2" # must be something like *.c
+echo "Extension: ${EXTENSION}"
+
+WRONG_NAME="$3"
+echo "Wrong name: ${WRONG_NAME}"
+
+CORRECT_NAME="$4"
+echo "Correct name: ${CORRECT_NAME}"
+
+FILES=$(find "$DIR_NAME" -type f -name "$EXTENSION" | grep "$WRONG_NAME")
+
+count=0
+for file in ${FILES}
+do
+    echo "Renaming ${file}..."
+    new_name="${file//$WRONG_NAME/$CORRECT_NAME}"
+    mv ${file} ${new_name}
+    ((count++))
+done
+
+echo "${count} files renamed."
