ec6e37746652f72af906fa042548dcfc26027383
alcam92 <alancamargo92@yahoo.co.uk>
2017-06-18 22:11:20 -0300

    Finished new_git_repo

--- new_git_repo.sh
+++ new_git_repo.sh
@@ -4,6 +4,15 @@
 #!/bin/bash
 
 if [ "$#" != 1 ]; then
-    echo "You must pass the path to the remote repository you are creating."
+    echo "You must provide the remote path."
+    exit
 fi
+
+REMOTE_PATH=$1
+echo "The remote path is ${REMOTE_PATH}."
+
+git init
+
+git remote add origin ${REMOTE_PATH}
+echo "Remote repository path set to ${REMOTE_PATH}."
 
